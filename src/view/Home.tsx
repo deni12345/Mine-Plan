16b33374191a8bb3bb10c8d86155b0c08062a137
liemtx <bebu1407@gmail.com>
2024-12-20 00:41:37 +0700

    LT#Udpate UI

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,27 +1,15 @@
 import {
-  Divider,
-  Drawer,
   IconButton,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
+  Stack,
+  StackProps,
   styled,
   Toolbar,
   Typography,
-  useTheme,
 } from "@mui/material";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Grid from "@mui/material/Grid2";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
-
-const drawerWidth = 240;
+import { MuiDrawer } from "./MuiDrawer";
 
 const Home = () => {
   const [open, setOpen] = useState(false);
@@ -36,17 +24,22 @@ const Home = () => {
 
   return (
     <Grid container direction={"column"} alignContent={"center"}>
-      <Grid>
-        <AppBar open={open} position="fixed">
+      <Grid flex={1} width={"100%"}>
+        <AppBar open={open}>
           <Toolbar>
-            <Typography variant="h6" noWrap flexGrow={1} component="div">
+            <Typography
+              variant="h6"
+              flex={1}
+              component="div"
+              textAlign="center"
+            >
               Persistent drawer
             </Typography>
             <IconButton
               color="inherit"
               aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              marginRight={2}
+              onClick={open ? handleDrawerClose : handleDrawerOpen}
+              sx={{ marginRight: "12px", position: "absolute", right: 20 }}
             >
               <MenuIcon />
             </IconButton>
@@ -62,72 +55,11 @@ const Home = () => {
 
 export default Home;
 
-interface AppBarProps extends MuiAppBarProps {
+interface AppBarProps extends StackProps {
   open?: boolean;
 }
 
-interface MuiDrawerProps {
-  open?: boolean;
-  handleDrawerClose?: () => void;
-}
-
-const MuiDrawer = (props: MuiDrawerProps) => {
-  const theme = useTheme();
-  return (
-    <Drawer
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: drawerWidth,
-          boxSizing: "border-box",
-        },
-      }}
-      zIndex={10}
-      variant="persistent"
-      anchor="left"
-      open={props.open}
-    >
-      <DrawerHeader>
-        <IconButton onClick={props.handleDrawerClose}>
-          {theme.direction === "ltr" ? (
-            <ChevronLeftIcon />
-          ) : (
-            <ChevronRightIcon />
-          )}
-        </IconButton>
-      </DrawerHeader>
-      <Divider />
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Drawer>
-  );
-};
-
-const AppBar = styled(MuiAppBar, {
+const AppBar = styled(Stack, {
   shouldForwardProp: (prop) => prop !== "open",
 })<AppBarProps>(({ theme }) => ({
   transition: theme.transitions.create(["margin", "width"], {
@@ -140,20 +72,11 @@ const AppBar = styled(MuiAppBar, {
       style: {
         width: "100%",
         marginLeft: "10px",
-        transition: theme.transitions.create(["margin", "width"], {
+        transition: theme.transitions.create(["margin"], {
           easing: theme.transitions.easing.easeOut,
           duration: theme.transitions.duration.enteringScreen,
         }),
       },
     },
   ],
-}));
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
 }));
---
+++ src/view/MuiDrawer.tsx
@@ -0,0 +1,88 @@
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import { styled } from "@mui/material/styles";
+import {
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
+
+const drawerWidth = 240;
+
+interface MuiDrawerProps {
+  open?: boolean;
+  handleDrawerClose?: () => void;
+}
+
+export const MuiDrawer = (props: MuiDrawerProps) => {
+  const theme = useTheme();
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+          boxSizing: "border-box",
+        },
+        zIndex: 10,
+      }}
+      variant="persistent"
+      anchor="left"
+      open={props.open}
+    >
+      <DrawerHeader>
+        <IconButton onClick={props.handleDrawerClose}>
+          {theme.direction === "ltr" ? (
+            <ChevronLeftIcon />
+          ) : (
+            <ChevronRightIcon />
+          )}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
